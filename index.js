09c66af5b527e0d71e4a37b4136a33dbc20c75ba
Suraj Navale <surajnawale4056@gmail.com>
2026-04-27 13:37:48 +0530

    Refactor chat application: enhance server setup with Socket.io event handling and improve HTML structure and styling for better user experience

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import http from 'node:http';
-import { Server} from 'socket.io';
+import { Server } from 'socket.io';
 import path from 'node:path';
 import express from 'express';
 
-async function main(){
+async function main() {
+  const app = express();
+  app.use(express.static(path.resolve('./public')));
 
-    const app = express();
-    app.use(express.static(path.resolve('./public')));
-    const server = http.createServer(app);
-    const io = new Server();
+  const server = http.createServer(app);
+  const io = new Server(server);
 
-    io.attach(server);
 
-    server.listen(9000 , ()=>{
-        console.log(`HTTP server is running on PORT 9000`)
+    
+
+  io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg); // broadcast to everyone
     });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+    });
+  });
+
+  server.listen(9000, () => {
+    console.log('HTTP server is running on PORT 9000');
+  });
 }
 
 main();
